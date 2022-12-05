c8a60737a1be49d8469d38a27e5e4b1e1d4836fe
Ellen Woodward <S00222321@atu.ie>
2022-12-05 10:37:52 +0000

    Create all players

--- 2020_Assessment/MainWindow.xaml.cs
+++ 2020_Assessment/MainWindow.xaml.cs
@@ -20,9 +20,78 @@ namespace _2020_Assessment
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Player> allPlayers = new List<Player>();
+        List<Player> selectedPlayers = new List<Player>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private List<Player> CreatePlayers()
+        {
+            List<Player> players = new List<Player>();
+
+            Random rnd = new Random();
+
+            string[] firstNames = {
+                "Adam", "Amelia", "Ava", "Chloe", "Conor", "Daniel", "Emily",
+                "Emma", "Grace", "Hannah", "Harry", "Jack", "James",
+                "Lucy", "Luke", "Mia", "Michael", "Noah", "Sean", "Sophie"};
+
+            string[] lastNames = {
+                "Brennan", "Byrne", "Daly", "Doyle", "Dunne", "Fitzgerald", "Kavanagh",
+                "Kelly", "Lynch", "McCarthy", "McDonagh", "Murphy", "Nolan", "O'Brien",
+                "O'Connor", "O'Neill", "O'Reilly", "O'Sullivan", "Ryan", "Walsh"
+            };
+
+            Position currentPosition = Position.Goalkeeper;
+
+            for (int i = 0; i < 18; i++)
+            {
+                // generate random date of birth
+                DateTime d1 = DateTime.Now.AddYears(-30);
+                DateTime d2 = DateTime.Now.AddYears(-20);
+
+                int range = (d2 - d1).Days;
+                DateTime dob = d1.AddDays(rnd.Next(range));
+
+                // generate set number of positions
+
+                switch (i)
+                {
+                    case 2:
+                        currentPosition = Position.Defender;
+                        break;
+                    case 8:
+                        currentPosition = Position.Midfielder;
+                        break;
+                    case 14:
+                        currentPosition = Position.Forward;
+                        break;
+                }
+
+                Player P = new Player()
+                {
+                    FirstName = firstNames[rnd.Next(20)],
+                    Surname = lastNames[rnd.Next(20)],
+                    DOB = dob,  
+                    PreferedPositon = currentPosition
+                };
+
+                players.Add(P);
+            }
+
+            return players;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create Players
+            allPlayers = CreatePlayers();
+
+            // Display in listbox
+            lbxAllPlayers.ItemsSource = allPlayers;
         }
     }
 }
